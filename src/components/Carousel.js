8a786e517f6d94b3e83ea70b32230b08eb3ac728
Alexander Williamson <alex@alexw.co.uk>
2019-08-07 12:01:23 +0100

    Image now fluid using gatsby data

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,19 +1,82 @@
 import React from "react"
-import Zoom from "../images/zoom.jpg"
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 import "./Carousel.Module.css"
 
 class Carousel extends React.Component {
-  render() {
+  // <img alt="Alex and Jess" src={Wind} />
+  // <img alt="Alex and Jess" src={Boat} />
+  // <img alt="Alex and Jess" src={Photo} />
+  // <img alt="Alex and Jess" src={Booth} />
+  // <img alt="Alex and Jess" src={Lounge} />*/}
+
+  render(data) {
+    console.info("data: " + JSON.stringify(data))
+    console.info("props: " + JSON.stringify(this.props))
     return (
-      <div className="slideshow">
-        <img alt="Alex and Jess" src={Zoom} />
-        {/*<img alt="Alex and Jess" src={Zoom} />
-        <img alt="Alex and Jess" src={Wind} />
-        <img alt="Alex and Jess" src={Boat} />
-        <img alt="Alex and Jess" src={Photo} />
-        <img alt="Alex and Jess" src={Booth} />
-        <img alt="Alex and Jess" src={Lounge} />*/}
-      </div>
+      <section className="slideshow">
+        <StaticQuery
+          query={graphql`
+            query {
+              zoom: file(relativePath: { eq: "zoom.jpg" }) {
+                relativePath
+                childImageSharp {
+                  fluid(maxWidth: 700, maxHeight: 700, cropFocus: CENTER) {
+                    ...GatsbyImageSharpFluid_tracedSVG
+                  }
+                }
+              }
+              wind: file(relativePath: { eq: "wind.jpg" }) {
+                relativePath
+                childImageSharp {
+                  fluid(maxWidth: 700, maxHeight: 700, cropFocus: CENTER) {
+                    ...GatsbyImageSharpFluid_tracedSVG
+                  }
+                }
+              }
+              boat: file(relativePath: { eq: "boat.jpg" }) {
+                relativePath
+                childImageSharp {
+                  fluid(maxWidth: 700, maxHeight: 700, cropFocus: CENTER) {
+                    ...GatsbyImageSharpFluid_tracedSVG
+                  }
+                }
+              }
+              photo: file(relativePath: { eq: "photo.jpg" }) {
+                relativePath
+                childImageSharp {
+                  fluid(maxWidth: 700, maxHeight: 700, cropFocus: CENTER) {
+                    ...GatsbyImageSharpFluid_tracedSVG
+                  }
+                }
+              }
+              booth: file(relativePath: { eq: "booth.jpg" }) {
+                relativePath
+                childImageSharp {
+                  fluid(maxWidth: 700, maxHeight: 700, cropFocus: CENTER) {
+                    ...GatsbyImageSharpFluid_tracedSVG
+                  }
+                }
+              }
+              lounge: file(relativePath: { eq: "lounge.jpg" }) {
+                relativePath
+                childImageSharp {
+                  fluid(maxWidth: 700, maxHeight: 700, cropFocus: CENTER) {
+                    ...GatsbyImageSharpFluid_tracedSVG
+                  }
+                }
+              }
+            }
+          `}
+          render={data => (
+            <Img
+              title="Header image"
+              alt="Greek food laid out on table"
+              fluid={data.boat.childImageSharp.fluid}
+            />
+          )}
+        />
+      </section>
     )
   }
 }
